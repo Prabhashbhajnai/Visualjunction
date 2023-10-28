1ee296577298ae70294225ad97fe82eeca238a37
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-10-28 15:06:22 +0530

    added layout files and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,37 @@
+import { Routes, Route } from 'react-router-dom'
+
 // styles
 import './globals.css'
 
+// Layouts
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+
+// components
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+/* This is named import due to index.ts in _root/pages */
+import { Home } from './_root/pages'
+
 const App = () => {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </>
-    )
+  return (
+    <>
+      <main className='flex h-screen'>
+        <Routes>
+          {/* public routes visible for everyone */}
+          <Route element={<AuthLayout />}>
+            <Route path='/sign-in' element={<SigninForm />} />
+            <Route path='/sign-up' element={<SignupForm />} />
+          </Route>
+
+          {/* private routes visible on after signing in*/}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </main>
+    </>
+  )
 }
 
 export default App
