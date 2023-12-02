2d942058434f6659632f6dbbd7a3bb953930ddc0
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-12-02 18:00:33 +0530

    bottom bar added for small devices

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ import { Toaster } from "@/components/ui/toaster"
 
 // Pages
 /* This is named import due to index.ts in _root/pages */
-import { Home } from './_root/pages'
+import { AllUsers, CreatePost, EditPost, Explore, Home, Profile, Saved, UpdateProfile } from './_root/pages'
 
 const App = () => {
   return (
@@ -30,6 +30,13 @@ const App = () => {
           {/* private routes visible on after signing in*/}
           <Route element={<RootLayout />}>
             <Route index element={<Home />} />
+            <Route path='/explore' element={<Explore />} />
+            <Route path='/saved' element={<Saved />} />
+            <Route path='/all-users' element={<AllUsers />} />
+            <Route path='/create-post' element={<CreatePost />} />
+            <Route path='/update-post/:id' element={<EditPost />} />
+            <Route path='/posts/:id/*' element={<Profile />} />
+            <Route path='/update-profile/:id' element={<UpdateProfile />} />
           </Route>
         </Routes>
         <Toaster />
