d1a2d1011d0fb47627d735e327f6b1ca2c47d31d
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-12-05 15:26:54 +0530

    Like and save api added and implemented

--- client/src/components/shared/PostStats.tsx
+++ client/src/components/shared/PostStats.tsx
@@ -1,4 +1,11 @@
+import React, { useState, useEffect } from "react"
 import { Models } from "appwrite"
+
+// Query and Mutation
+import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from "@/lib/react-query/queriesAndMutations"
+
+import { checkIsLiked } from "@/lib/utils"
+import Loader from "./Loader"
 
 type PostStatsProps = {
     post: Models.Document
@@ -6,6 +13,55 @@ type PostStatsProps = {
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
+    const likesList = post.likes.map((user: Models.Document) => user.$id)
+
+    const [likes, setLikes] = useState(likesList)
+    const [isSaved, setIsSaved] = useState(false)
+
+    const { mutate: likePost } = useLikePost()
+    const { mutate: savePost, isPending: isSavingPost } = useSavePost()
+    const { mutate: deleteSavedPost, isPending: isDeletingSaved } = useDeleteSavedPost()
+
+    const { data: currentUser } = useGetCurrentUser()
+
+    const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post.$id)
+
+    useEffect(() => {
+        setIsSaved(!!savedPostRecord)
+    }, [currentUser])
+
+    const handleLikePost = (e: React.MouseEvent) => {
+        e.stopPropagation()
+
+        let newLikes = [...likes]
+
+        const hasLiked = newLikes.includes(userId)
+
+        if (hasLiked) {
+            newLikes = newLikes.filter((id) => id !== userId)
+        } else {
+            newLikes.push(userId)
+        }
+
+        setLikes(newLikes)
+        likePost({ postId: post.$id, likesArray: newLikes })
+    }
+
+    const handleSavePost = (e: React.MouseEvent) => {
+        e.stopPropagation()
+
+
+
+        if (savedPostRecord) {
+            setIsSaved(false)
+            deleteSavedPost(savedPostRecord.$id)
+        } else {
+            savePost({ postId: post.$id, userId })
+            setIsSaved(true)
+        }
+
+    }
+
     return (
         <>
             <div className="flex justify-between items-center z-10">
@@ -13,26 +69,32 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
                 {/* Like icon and counter */}
                 <div className="flex gap-2 mr-5">
                     <img
-                        src="/assets/icons/like.svg"
+                        src={checkIsLiked(likes, userId) ?
+                            "/assets/icons/liked.svg" :
+                            "/assets/icons/like.svg"
+                        }
                         alt="like"
                         width={20}
                         height={20}
-                        onClick={() => { }}
+                        onClick={handleLikePost}
                         className="cursor-pointer"
                     />
-                    <p className="small-medium lg:base-medium">0</p>
+                    <p className="small-medium lg:base-medium">{likes.length}</p>
                 </div>
 
                 {/* Save icon */}
                 <div className="flex gap-2">
-                    <img
-                        src="/assets/icons/save.svg"
+                    {isSavingPost || isDeletingSaved ? <Loader /> : <img
+                        src={isSaved ?
+                            "/assets/icons/saved.svg" :
+                            "/assets/icons/save.svg"
+                        }
                         alt="save"
                         width={20}
                         height={20}
-                        onClick={() => { }}
+                        onClick={handleSavePost}
                         className="cursor-pointer"
-                    />
+                    />}
                 </div>
             </div>
         </>
--- client/src/lib/react-query/queriesAndMutations.ts
+++ client/src/lib/react-query/queriesAndMutations.ts
@@ -1,7 +1,7 @@
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query'
 
 // API
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api'
 
 // Types
 import { INewPost, INewUser } from '@/types'
@@ -47,5 +47,72 @@ export const useGetRecentPosts = () => {
     return useQuery({
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
         queryFn: getRecentPosts
+    })
+}
+
+export const useLikePost = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: ({ postId, likesArray }: { postId: string, likesArray: string[] }) => likePost(postId, likesArray),
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+            })
+        }
+    })
+}
+
+export const useSavePost = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: ({ postId, userId }: { postId: string, userId: string }) => savePost(postId, userId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+            })
+        }
+    })
+}
+
+export const useDeleteSavedPost = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_POSTS]
+            })
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+            })
+        }
+    })
+}
+
+export const useGetCurrentUser = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        queryFn: getCurrentUser
     })
 }
